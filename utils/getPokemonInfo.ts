16e432a1e074978e7cd659d941b353eb996eb453
Adalberto <adalberto.devs@gmail.com>
2022-05-30 18:08:29 +0100

    Course End

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -1,12 +1,18 @@
 import { pokeApi } from '../api';
 import { Pokemon, PokemonData } from '../interfaces';
 
-export const getPokemonInfo = async (nameOrId: string): Promise<Pokemon> => {
-  const { data } = await pokeApi.get<PokemonData>(`/pokemon/${nameOrId}`);
+export const getPokemonInfo = async (
+  nameOrId: string
+): Promise<Pokemon | null> => {
+  try {
+    const { data } = await pokeApi.get<PokemonData>(`/pokemon/${nameOrId}`);
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites
-  };
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites
+    };
+  } catch (err) {
+    return null;
+  }
 };
